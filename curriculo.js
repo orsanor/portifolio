f03ae60c0bcc8680313e9771260be66b713433ce
Jonathan Orsano <orsanor@gmail.com>
2025-02-18 16:48:57 -0300

    removendo comentarios

--- curriculo.js
+++ curriculo.js
@@ -2,22 +2,18 @@ function downloadCV() {
     const button = document.querySelector('.btn button');
     const loaderText = document.querySelector('.loader-text');
     
-    // Inicia animação de carregamento
     button.classList.add('loading');
     
-    // Simula processo de download
     setTimeout(() => {
         const cvUrl = 'assets/Curriculo Jonathan Orsano.pdf';
         const link = document.createElement('a');
         link.href = cvUrl;
         link.download = 'Jonathan-Orsano-CV.pdf';
         
-        // Inicia o download
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
         
-        // Volta ao estado normal
         clearInterval(messageInterval);
         button.classList.remove('loading');
         loaderText.style.opacity = '0';
